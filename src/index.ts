8443f4e6ac0d5da532b6cb354025857d355fe255
Andrew Crites <acrites@mobiquityinc.com>
2018-11-21 22:01:51 -0500

    feat(titles): Adding titles from URL contents to replace links in markup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,40 @@
 import * as MarkdownIt from 'markdown-it';
 import * as taskLists from 'markdown-it-task-lists';
-import { readFileSync, writeFileSync, mkdirSync } from 'fs';
+import { JSDOM } from 'jsdom';
+import * as mkdirp from 'mkdirp-promise';
+const fsPromises = require('fs').promises;
+const { readFile, writeFile } = fsPromises;
 
-const md = new MarkdownIt({
-  linkify: true,
-});
+(async () => {
+  const md = new MarkdownIt({
+    linkify: true,
+  });
 
-const parser = md.use(taskLists);
+  const parser = md.use(taskLists);
 
-mkdirSync('build');
-writeFileSync(
-  'build/index.html',
-  parser.render(readFileSync('README.md', 'utf-8')),
-);
+  await mkdirp('build');
+  const html = parser.render(await readFile('README.md', 'utf-8'));
+  const dom = new JSDOM(html);
+  const doc = dom.window.document;
+
+  await Promise.all(
+    [].slice.call(doc.querySelectorAll('a[href]')).map(async elem => {
+      const url = elem.textContent.trim();
+      try {
+        const contentDom = await JSDOM.fromURL(url);
+        const contentDoc = contentDom.window.document;
+
+        const title = contentDoc.querySelector('title');
+        if (title && title.textContent.trim()) {
+          elem.textContent = title.textContent.trim();
+        }
+      } catch {}
+    }),
+  );
+
+  const head = doc.querySelector('head');
+  const metaCharset = doc.createElement('meta');
+  metaCharset.setAttribute('charset', 'UTF8');
+  head.appendChild(metaCharset);
+  await writeFile('build/index.html', dom.serialize());
+})();
